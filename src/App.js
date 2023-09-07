ae4dacfde004a065766bcb52181ca2b308883cc3
yhammix <mansilla.y86@gmail.com>
2023-09-07 16:01:27 -0300

    agregando primer componente Card

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div style={{padding: "50px"}}>
+      <h1 style={{marginBottom:"20px"}}>Kanban UI</h1>
+    </div>
+  );
+}
+
+export default App;
